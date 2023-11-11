b713129264e01320f96e9193f400047888e0e152
duggurd <alexanderhaugerud@gmail.com>
2023-11-11 01:07:17 -0800

    Changes

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,10 @@
-use std::error::Error;
-use std::iter::Cloned;
-use std::{env::args, fs};
-use std::fs::OpenOptions;
-use std::io::{Write};
+use doit::doit::Doit;
 
-use doit::Doit;
-use doit::MainCmd;
-
-fn main() {
-    let argv = args();
-    let arg_s: Vec<String> = argv.collect();
-    
-    let main_cmd = MainCmd::from_str(&arg_s[1]);
-
+fn main() {    
     let mut doit = Doit::default();
-    doit.parse();
     
-    match main_cmd {
-        MainCmd::Task => {
-            if arg_s.len() <= 2 {
-                panic!("Not enough arguments in task");
-            } else {
-                doit.add_task(arg_s[2].to_string(),  arg_s.get(3).cloned() );
-            }
-        }
-        MainCmd::Stage => {println!("stagin task X")}
-        MainCmd::Done => {println!("Done with task X")}
-        MainCmd::List => {println!("Listing tasks")}
+    match doit.run() {
+        Ok(()) => {}
+        Err(e) => { eprintln!("{}", e) }
     }
 }
-
-
-
-
-
-
